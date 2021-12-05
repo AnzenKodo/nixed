b4bd28344c40d081197436b4dde2207177c4a685
AnzenKodo <AnzenKodo@altmails.com>
2021-12-05 09:29:08 +0530

    ⬆ Update home-manager.nix

--- nixos.nix
+++ nixos.nix
@@ -103,12 +103,12 @@
     };
   };
 
-  programs.ssh.startAgent = true;
-  # programs.gnupg.agent = {
-  #       enable = true;
-  #       enableSSHSupport = true;
-  #       # pinentryFlavor = "gnome3";
-  #     };
+  # programs.ssh.startAgent = true;
+  programs.gnupg.agent = {
+        enable = true;
+        enableSSHSupport = true;
+        # pinentryFlavor = "gnome3";
+      };
 
   # Gnome Keyring
   services.gnome3.gnome-keyring.enable = true;
