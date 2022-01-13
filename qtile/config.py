674681292db9d73ff2049c75db9bf2405a5bac0e
AnzenKodo <AnzenKodo@altmails.com>
2022-01-14 05:22:53 +0530

    ⬆ Update qtile/__pycache__/config.cpython-37.pyc qtile/config.py

--- qtile/config.py
+++ qtile/config.py
@@ -234,15 +234,15 @@ mouse = [
     )
 ]
 
-# floating_layout = layout.Floating(float_rules=[
-#     # *layout.Floating.default_float_rules,
-#     Match(wm_class='confirmreset'),  # gitk
-#     Match(wm_class='makebranch'),  # gitk
-#     Match(wm_class='maketag'),  # gitk
-#     Match(wm_class='ssh-askpass'),  # ssh-askpass
-#     Match(title='branchdialog'),  # gitk
-#     Match(title='pinentry'),  # GPG key password entry
-# ])
+floating_layout = layout.Floating(float_rules=[
+    # *layout.Floating.default_float_rules,
+    Match(wm_class='confirmreset'),  # gitk
+    Match(wm_class='makebranch'),  # gitk
+    Match(wm_class='maketag'),  # gitk
+    Match(wm_class='ssh-askpass'),  # ssh-askpass
+    Match(title='branchdialog'),  # gitk
+    Match(title='pinentry'),  # GPG key password entry
+])
 
 widget_defaults = dict(
     background=Style.background,
