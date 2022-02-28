7b9c0501f932697f56df36c980adceb9a8723555
Tesura <AnzenKodo@altmails.com>
2022-02-28 13:50:18 +0530

    ⬆ Update qtile/bar.py

--- qtile/bar.py
+++ qtile/bar.py
@@ -32,71 +32,71 @@ screens = [
         top=bar.Bar(
             [
                 widget.CurrentLayoutIcon(),
-        #         widget.GroupBox(
-        #             # inactive=Style.fontColor,
-        #             # this_current_screen_border=Style.heighlight,
-        #             # urgent_border=Style.important,
-        #             padding=0,
-        #             highlight_method='block',
-        #             rounded=False,
-        #             hide_unused=True,
-        #             disable_drag=True,
-        #             use_mouse_wheel=False
-        #         ),
-        #         # widget.Prompt(
-        #         #     foreground=Style.fontColor,
-        #         # ),
-        #         # widget.Chord(
-        #         #     chords_colors={
-        #         #         'launch': ('#ff0000', '#ffffff'),
-        #         #     },
-        #         #     name_transform=lambda name: name.upper(),
-        #         # ),
-        #         widget.Notify(
-        #             # background=Style.line,
-        #             # foreground=Style.fontColor,
-        #             # foreground_low=Style.other,
-        #             # foreground_urgent=Style.important,
-        #             fmt=' 💬 {} ',
-        #             fontsize=15,
-        #             action=False,
-        #             # parse_text=format_text
-        #         ),
-        #         widget.Spacer(
-        #             # background=Style.line
-        #         ),
-        #         widget.Systray(),
-        #         # widget.Pomodoro(
-        #         #     fmt=' {} ',
-        #         #     color_active=Colors.green,
-        #         #     color_break=Style.red,
-        #         #     prefix_paused='⏸️',
-        #         #     prefix_inactive='🍅',
-        #         #     prefix_break='🧘',
-        #         #     prefix_active='👨‍💻',
-        #         #     prefix_long_break='🎉',
-        #         # ),
-        #         widget.Volume(
-        #             fmt=' 🔊 {} |',
-        #             # emoji=True,
-        #         ),
-        #         widget.Backlight(
-        #             # backlight_name=Commands.backlight_name,
-        #             format=' ☀️ {percent:1.0%} |',
-        #         ),
-        #         widget.Battery(
-        #             # foreground=Style.fontColor,
-        #             # background=Style.background,
-        #             # low_background=Style.important,
-        #             format=' {char} {percent:2.0%} ',
-        #             notify_below=30,
-        #             discharge_char='🔋',
-        #             charge_char='⚡',
-        #             full_char='🔌',
-        #             empty_char='',
-        #             unknown_char='',
-        #             show_short_text=False
-        #         ),
+                widget.GroupBox(
+                    inactive=Style.fontColor,
+                    this_current_screen_border=Style.heighlight,
+                    urgent_border=Style.important,
+                    padding=0,
+                    highlight_method='block',
+                    rounded=False,
+                    hide_unused=True,
+                    disable_drag=True,
+                    use_mouse_wheel=False
+                ),
+                widget.Prompt(
+                    foreground=Style.fontColor,
+                ),
+                # widget.Chord(
+                #     chords_colors={
+                #         'launch': ('#ff0000', '#ffffff'),
+                #     },
+                #     name_transform=lambda name: name.upper(),
+                # ),
+                widget.Notify(
+                    background=Style.line,
+                    foreground=Style.fontColor,
+                    foreground_low=Style.other,
+                    foreground_urgent=Style.important,
+                    fmt=' 💬 {} ',
+                    fontsize=15,
+                    action=False,
+                    # parse_text=format_text
+                ),
+                widget.Spacer(
+                    background=Style.line
+                ),
+                widget.Systray(),
+                # widget.Pomodoro(
+                #     fmt=' {} ',
+                #     color_active=Colors.green,
+                #     color_break=Style.red,
+                #     prefix_paused='⏸️',
+                #     prefix_inactive='🍅',
+                #     prefix_break='🧘',
+                #     prefix_active='👨‍💻',
+                #     prefix_long_break='🎉',
+                # ),
+                widget.Volume(
+                    fmt=' 🔊 {} |',
+                    # emoji=True,
+                ),
+                widget.Backlight(
+                    # backlight_name=Commands.backlight_name,
+                    format=' ☀️ {percent:1.0%} |',
+                ),
+                widget.Battery(
+                    foreground=Style.fontColor,
+                    background=Style.background,
+                    low_background=Style.important,
+                    format=' {char} {percent:2.0%} ',
+                    notify_below=30,
+                    discharge_char='🔋',
+                    charge_char='⚡',
+                    full_char='🔌',
+                    empty_char='',
+                    unknown_char='',
+                    show_short_text=False
+                ),
                 widget.Memory(
                     format='| 📊{MemUsed: .0f}MB |',
                 ),
