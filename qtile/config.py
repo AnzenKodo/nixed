9dc82646440c781d4b7dd4548190a31a5d1c2b57
Tesura <AnzenKodo@altmails.com>
2022-03-01 17:36:52 +0530

    ⬆ Update fish.fish qtile/__pycache__/config.cpython-39.pyc qtile/modules/__pycache__/__init__.cpython-39.pyc qtile/modules/__pycache__/advance.cpython-39.pyc qtile/modules/__pycache__/hooks.cpython-39.pyc qtile/modules/__pycache__/layout.cpython-39.pyc qtile/modules/__pycache__/mouse.cpython-39.pyc qtile/modules/__pycache__/screen.cpython-39.pyc qtile/modules/__pycache__/shortcuts.cpython-39.pyc qtile/variables/__pycache__/__init__.cpython-39.pyc qtile/variables/__pycache__/commands.cpython-39.pyc qtile/variables/__pycache__/style.cpython-39.pyc

--- qtile/config.py
+++ qtile/config.py
@@ -4,7 +4,7 @@ from modules.screen import screens, widget_defaults
 from modules.shortcuts import keys, groups
 from modules.mouse import mouse
 from modules.hooks import *
-import sys
+# import sys
 
-sys.dont_write_bytecode = True
+# sys.dont_write_bytecode = True
 extension_defaults = widget_defaults.copy()
