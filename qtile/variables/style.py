258d167a40ccd9ef2a17569ee99cf88e518ef2dc
Tesura <AnzenKodo@altmails.com>
2022-03-07 00:31:39 +0530

    ⬆ Update qtile/variables/__pycache__/style.cpython-39.pyc qtile/variables/style.py

--- qtile/variables/style.py
+++ qtile/variables/style.py
@@ -4,7 +4,7 @@ import os
 # Loads generated colors from pywal
 cache = f'/home/{os.getlogin()}/.cache/wal/colors'
 # Default colors from Punk Colors #170327
-colors = ['#fefefe', '#01a252', '#f2b705', '#0583f2', '#36a598', '#05f2c7', '#170327', '#310273']
+colors = ['#170327', '#01a252', '#f2b705', '#0583f2', '#36a598', '#05f2c7', '#310273', '#fefbfe']
 # colors = []
 file_exists = os.path.exists(cache)
 
